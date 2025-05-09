43c2252859e97e3d84067a6bcc3f157aa637ce33
martynas <marat19@gmail.com>
2025-05-09 22:22:38 +0300

    last

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,31 +5,7 @@ const {
   randomYear,
   genderSelector,
 } = require("../support/random_data");
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
 const user = {
   name: faker.person.fullName(),
   email: faker.internet.email(),
