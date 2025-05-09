117f5e539968415556f5947ce8a2000f3a76f2b8
martynas <marat19@gmail.com>
2025-05-09 14:04:41 +0300

    upd yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mocha-junit-reporter",
+  reporterOptions: {
+    mochaFile: "cypress/results/results-[hash].xml",
+    toConsole: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: "http://automationexercise.com",
